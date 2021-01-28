7a607af811ce2fbac8cbab3ed19a54ea23223d61
Tre Olson <treolson@live.com>
2021-01-28 11:15:25 -0600

    completed form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import {useState} from 'react';
+import Form from '../Form/Form';
 
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
+const [newItem, setNewItem] = useState('');
+const [newQuantity, setNewQuantity] = useState(0);
+const [newUnit, setNewUnit] = useState('');
+
 function App() {
     return (
         <div className="App">
             <Header />
+            <Form 
+            newItem={newItem}
+            setNewItem={setNewItem}
+            newQuantity={newQuantity}
+            setNewQuantity={setNewQuantity}
+            newUnit={newUnit}
+            setNewUnit={setNewUnit}
+            handleSubmit={handleSubmit}
+            />
             <main>
                 <p>Under Construction...</p>
             </main>
